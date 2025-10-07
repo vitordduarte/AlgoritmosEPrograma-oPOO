5871560c9c444f7a28b79d9a7dd079914c764a91
vrfrohlich <73611733+vrfrohlich@users.noreply.github.com>
2025-10-06 22:27:53 -0300

    Update main.cpp

--- TrabalhoGA/main.cpp
+++ TrabalhoGA/main.cpp
@@ -1,1 +1,26 @@
 
+/*
+
+Tabela 1 – Tela Inicial
+1 - novo album
+2 Acessar Álbum 
+3 Sair do Aplicativo
+
+
+Tabela 2 – Tela Gerenciar Álbum
+1 Ver Álbum 
+2 Gerenciar a coleção 
+3 Abrir pacote de Figurinhas
+4 Voltar ao menu Anterior
+
+Tabela 3 – Tela Gerenciar Coleção
+Código Opção Descrição
+1 Colar Figurinha
+2 Disponibilizar para
+troca
+3 Propor troca de figurinhas
+4 Revisar solicitações
+de troca
+5 Voltar ao menu
+Anterior
+*/
