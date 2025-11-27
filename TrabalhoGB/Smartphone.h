b8c0f8a497b724a160c28f7f9b29e0253189064e
vitordduarte <vitordduarte05@gmail.com>
2025-11-27 20:18:20 -0300

    Update Smartphone.h

--- TrabalhoGB/Smartphone.h
+++ TrabalhoGB/Smartphone.h
@@ -4,16 +4,16 @@
 
 using namespace std;
 
-Smartphone::Smartphone(const string &n, float p, int e, float w, int v, int g, int ar, int mar) // aqui ele herda de eletronico que herda de produto
-    : Eletronico(n,p,e,w,v,g), armazenamento(ar), marca(cam) {}
+Smartphone::Smartphone(const string &n, float p, int e, float w, int v, int g, int armaz, string marca) // aqui ele herda de eletronico que herda de produto
+    : Eletronico(n,p,e,w,t,g), armazenamento(armaz), marca(marca) {}
 
 Smartphone::~Smartphone() {}
 
-std::string Smartphone::getEspecificacoes() const {
-    return std::to_string(armazenamento) + "GB, " + std::to_string(camera) + "MP";
+string Smartphone::getEspecificacoes() const {
+    return to_string(armazenamento) + "GB, " + string(marca) + "é a marca";
 }
 
 void Smartphone::exibirInfo() const {
     Eletronico::exibirInfo();
-    std::cout << "  (Smartphone) " << getEspecificacoes() << "\n";
+    cout << "  (Smartphone) " << getEspecificacoes() << "\n";
 }
