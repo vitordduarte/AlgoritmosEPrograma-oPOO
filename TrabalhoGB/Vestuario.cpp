2f5943d7737990f2ad1bcd1977e88fcfec87db4e
vitordduarte <vitordduarte05@gmail.com>
2025-11-27 20:07:22 -0300

    Create Vestuario.cpp

---
+++ TrabalhoGB/Vestuario.cpp
@@ -0,0 +1,25 @@
+#include "Vestuario.h"
+#include <iostream>
+
+using namespace std;
+
+Vestuario::Vestuario(const string &n, float p, int e, float w, const string &t, const string &m)
+    : Produto(n,p,e,w), tamanho(t), material(m) {}
+
+Vestuario::~Vestuario() {}
+
+double Vestuario::calcularFrete() const {
+    return 5.0 + peso * 2.0;
+}
+
+void Vestuario::exibirInfo() const {
+    Produto::exibirInfo();
+    std::cout << "  (Vestuário) Tamanho: " << tamanho << ", Material: " << material << "\n";
+}
+
+string Vestuario::getTamanho() const {
+  return tamanho;
+}
+string Vestuario::getMaterial() const {
+  return material;
+}
