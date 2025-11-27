d8c8ce9016ada9d22d33e89764cbbd2b06f791da
vitordduarte <vitordduarte05@gmail.com>
2025-11-27 19:46:59 -0300

    Create Eletronico.h

---
+++ TrabalhoGB/Eletronico.h
@@ -0,0 +1,19 @@
+#ifndef ELETRONICO_H
+#define ELETRONICO_H
+#include "Produto.h"
+
+using namespace std;
+
+class Eletronico : public Produto {
+protected:
+    int tensao;
+    int garantiaMeses;
+public:
+    Eletronico(const string &n="", double p=0.0, int e=0, double w=0.0, int t=0, int g=0); //incluindo as 4 var de produto + 2 do eletronico
+    virtual ~Eletronico();
+    virtual float calcularFrete() const override;
+    virtual void exibirInfo() const override;
+    int gettensao() const;
+    int getGarantia() const;
+};
+#endif
