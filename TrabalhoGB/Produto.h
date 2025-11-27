2b0d9a7e9ffe167908c42796bb275fa91c4244f4
vitordduarte <vitordduarte05@gmail.com>
2025-11-27 19:38:09 -0300

    Create Produto.h

---
+++ TrabalhoGB/Produto.h
@@ -0,0 +1,17 @@
+#ifndef PRODUTO_H
+#define PRODUTO_H
+#include <string>
+class Produto {
+protected:
+    std::string nome;
+    float preco;
+    int estoque;
+    float peso;
+public:
+    Produto(const std::string &n="", float p=0.0, int e=0, float w=0.0);
+    virtual ~Produto();
+    virtual float calcularFrete() const = 0;
+    virtual void exibirInfo() const;
+    float getPreco() const;
+};
+#endif
