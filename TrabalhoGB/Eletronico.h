1c169de76bfdc33a5b9c9f7b8b242f9d8945a355
vitordduarte <vitordduarte05@gmail.com>
2025-11-27 19:50:11 -0300

    Update Eletronico.h

--- TrabalhoGB/Eletronico.h
+++ TrabalhoGB/Eletronico.h
@@ -9,7 +9,7 @@ protected:
     int tensao;
     int garantiaMeses;
 public:
-    Eletronico(const string &n="", double p=0.0, int e=0, double w=0.0, int t=0, int g=0); //incluindo as 4 var de produto + 2 do eletronico
+    Eletronico(const string &n="", float p=0.0, int e=0, float w=0.0, int t=0, int g=0); //incluindo as 4 var de produto + 2 do eletronico
     virtual ~Eletronico();
     virtual float calcularFrete() const override;
     virtual void exibirInfo() const override;
