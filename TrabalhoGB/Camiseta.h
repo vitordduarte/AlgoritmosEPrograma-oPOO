d8256374f690d613993d0e0ae21ac6a1b3570739
vitordduarte <vitordduarte05@gmail.com>
2025-11-27 21:31:17 -0300

    Update Camiseta.h

--- TrabalhoGB/Camiseta.h
+++ TrabalhoGB/Camiseta.h
@@ -6,7 +6,7 @@ using namespace std
 class Camiseta : public Vestuario {
     string estampa;
 public:
-    Camiseta(const string &nome1="", float preco1=0.0, int estoque1=0, float peso1=0.0, const string &tamanho1="", const string &m="", const std::string &est="");
+    Camiseta(const string &nome1="", float preco1=0.0, int estoque1=0, float peso1=0.0, const string &tamanho1="", const string &material1="", const string &estampa1="");
     virtual ~Camiseta();
     std::string getEstampa() const;
     virtual void exibirInfo() const override;
