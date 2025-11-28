99c6a6df42016b5588d0c52f76e1f2a6f1398340
vitordduarte <vitordduarte05@gmail.com>
2025-11-27 21:25:02 -0300

    Update Smartphone.h

--- TrabalhoGB/Smartphone.h
+++ TrabalhoGB/Smartphone.h
@@ -9,8 +9,9 @@ class Smartphone : public Eletronico {
     int armazenamento;
     string marca;
 public:
-    Smartphone(const string &n="", float p=0.0, int e=0, float w=0.0, int t=0, int g=0, int armaz=0, int &marca="");
+    Smartphone(const string &nome1="", float preco1=0.0, int estoque1=0, float peso1=0.0, int tensao1=0, int garantiaMeses1=0, int armazenamento1=0, string &marca1="");
     virtual ~Smartphone();
+
     string getEspecificacoes() const;
     virtual void exibirInfo() const override;
 };
